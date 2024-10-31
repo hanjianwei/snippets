514dc17d29ff709d53b05867a3073f5aebf5dc50
Jianwei Han <hanjianwei@gmail.com>
2024-10-31 13:40:29 +0800

    feat(app): Add nas ssl update script
    Changelog: feature

---
+++ update_nas_ssl/update_qnap_ssl.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env zsh
+
+export DEPLOY_SSH_USER="admin"
+export DEPLOY_SSH_KEYFILE="/etc/stunnel/stunnel.pem"
+export DEPLOY_SSH_CERTFILE="/etc/stunnel/stunnel.pem"
+export DEPLOY_SSH_CAFILE="/etc/stunnel/uca.pem"
+export DEPLOY_SSH_REMOTE_CMD="/etc/init.d/stunnel.sh restart"
+
+~/Applications/acme.sh/acme.sh --deploy -d q.hjw.ink --deploy-hook ssh
